bef461d8094313fa256c7b43a11823ad2282bda8
Faizan Shakeel <faizan.sg@gmail.com>
2015-12-14 08:56:03 +0100

    Implemented User logout/info Popover UI

--- js/controllers/global_tabs_and_panels.js
+++ js/controllers/global_tabs_and_panels.js
@@ -1,26 +1,190 @@
 
-var app = angular.module('globalTabsAndPanelsModule', ['ui.bootstrap']);
+var app = angular.module('mainViewModule', ['ui.bootstrap', 'nsPopover']);
 
-app.controller('TabsController', ['$scope', 'NewProjectService', 'NewTaskService', 'filterFilter', function ($scope, NewProjectService, NewTaskService, filterFilter)
+app.controller('Main_View_Controller', ['$scope', 'NewProjectService', 'NewTaskService', 'filterFilter', function ($scope, NewProjectService, NewTaskService, filterFilter)
 {
     var vm = this;
 
-//    vm.searchInputFieldText = NewProjectService.searchInputFieldText;
+    ////////////////// ACCORDION //////////////////
 
-//    GlobalViewService.testArray = vm.testArray;
+    vm.oneAtATime = true;
 
-//    vm.active = {
-//        'Projects': true,
-//        'Tasks': false,
-//        'Documents': false,
-//        'Users': false
+    vm.groups = [
+        {
+            title: 'Dynamic Group Header - 1',
+            content: 'Dynamic Group Body - 1'
+        },
+        {
+            title: 'Dynamic Group Header - 2',
+            content: 'Dynamic Group Body - 2'
+        }
+    ];
+
+    vm.status = {
+        isFirstOpen: true,
+        isFirstDisabled: false
+    };
+
+    vm.scrollBarConfig = {
+        autoResize: true // If true, will listen for DOM elements being added or removed inside the scroll container
+//        direction: 'vertical', // The direction of the scrollbar
+//        scrollbar: {
+//            width: 4, // Width (thickness. Is actually height on horizontal scrollbars) of the scrollbar
+//            hoverWidth: 14, // Width on scrollbar hover
+//            color: 'rgba(0,0,0, .6)', // Background color of the scrollbar
+//            show: false // If true, scrollbar will always be visible
+//        },
+//        scrollbarContainer: {
+//            width: 10, // Width of the container surrounding the scrollbar. Becomes visible on hover
+//            color: 'rgba(0,0,0, .1)' // Background color of the scrollbar container
+//        },
+//        scrollTo: null // Scroll to the 'start' or 'end' on initialization and content changes. Pixel values may also be given.
+    };
+
+    ////////////////// ACCORDION [END]//////////////////
+
+
+    ////////////////// CHATTING PANEL //////////////////
+
+    var online = 'fa fa-circle-o';
+    var offline = '';
+
+    vm.users = [
+
+        {
+            'name': 'User Name',
+            'status': offline
+        },
+        {
+            'name': 'User Name',
+            'status': online
+        },
+        {
+            'name': 'User Name',
+            'status': offline
+        },
+        {
+            'name': 'User Name',
+            'status': online
+        },
+        {
+            'name': 'User Name',
+            'status': offline
+        },
+        {
+            'name': 'User Name',
+            'status': online
+        },
+        {
+            'name': 'User Name',
+            'status': offline
+        },
+        {
+            'name': 'User Name',
+            'status': online
+        },
+        {
+            'name': 'User Name',
+            'status': offline
+        },
+        {
+            'name': 'User Name',
+            'status': online
+        },
+        {
+            'name': 'User Name',
+            'status': offline
+        },
+        {
+            'name': 'User Name',
+            'status': online
+        },
+        {
+            'name': 'User Name',
+            'status': offline
+        },
+        {
+            'name': 'User Name',
+            'status': online
+        },
+        {
+            'name': 'User Name',
+            'status': offline
+        },
+        {
+            'name': 'User Name',
+            'status': online
+        },
+        {
+            'name': 'User Name',
+            'status': offline
+        },
+        {
+            'name': 'User Name',
+            'status': online
+        },
+        {
+            'name': 'User Name',
+            'status': offline
+        },
+        {
+            'name': 'User Name',
+            'status': online
+        },
+        {
+            'name': 'User Name',
+            'status': offline
+        },
+        {
+            'name': 'User Name',
+            'status': online
+        },
+        {
+            'name': 'User Name',
+            'status': offline
+        },
+        {
+            'name': 'User Name',
+            'status': online
+        },
+        {
+            'name': 'User Name',
+            'status': offline
+        },
+        {
+            'name': 'User Name',
+            'status': online
+        },
+        {
+            'name': 'User Name',
+            'status': offline
+        },
+        {
+            'name': 'User Name',
+            'status': online
+        },
+        {
+            'name': 'User Name',
+            'status': offline
+        },
+        {
+            'name': 'User Name',
+            'status': online
+        },
+        {
+            'name': 'User Name',
+            'status': online
+        }
+
+    ];
+
+//    vm.scrollBarConfig = {
+//        autoResize: true // If true, will listen for DOM elements being added or removed inside the scroll container
 //    };
 
-//    vm.activateTab = function(tab) {
-//        console.log(tab);
-//        vm.active = {}; //reset
-//        vm.active[tab] = true;
-//    };
+    ////////////////// CHATTING PANEL [END]//////////////////
+
+    ////////////////// NAV BAR ////////////////////
 
     vm.searchDropdownStatus = {
         isOpen: false
@@ -32,21 +196,20 @@ app.controller('TabsController', ['$scope', 'NewProjectService', 'NewTaskService
 
         vm.selectedItem = item;
 
-//        vm.active = {}; //reset
-//        vm.active[item] = true;
-
-//        if(vm.selectedItem == 'Tasks')
-//        {
-//            console.log(vm.tabs[1].active);
-//            vm.tabs[1].active = true;
-//            console.log(vm.tabs[1].active);
-//        }
+        vm.active = {}; //reset
+        vm.active[item] = true;
 
     };
 
-    vm.panels = NewProjectService.panels;
 
-//    vm.panels = filterFilter(NewProjectService.panels, NewProjectService.searchInputFieldText);
+    vm.displayPopover = false;
+
+    ////////////////// NAV BAR [END] ////////////////////
+
+
+    ////////////////// TABS AND PANELS //////////////////
+
+    vm.panels = NewProjectService.panels;
 
     vm.taskPanels = NewTaskService.taskPanels;
 
@@ -90,27 +253,13 @@ app.controller('TabsController', ['$scope', 'NewProjectService', 'NewTaskService
         while(i--){
             if( arr[i]
                 && (arr[i][attr] === value) ){
-
                 arr.splice(i,1);
-
             }
         }
         return arr;
     };
 
-//    var removeByAttr = function(arr, attr, value, tasksProject){
-//        var i = arr.length;
-//        while(i--){
-//            if( arr[i]
-//                && arr[i].hasOwnProperty(attr)
-//                && (arguments.length > 2 && arr[i][attr] === value ) ){
-//
-//                arr.splice(i,1);
-//
-//            }
-//        }
-//        return arr;
-//    };
+    ////////////////// TABS AND PANELS [END]//////////////////
 
 }]);
 
